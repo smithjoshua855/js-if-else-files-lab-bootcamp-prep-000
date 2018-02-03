567d8c496568ffcef0fee2710c0ef0580a3ba005
Joshua Ben <jbdeuce14@aol.com>
2018-02-03 05:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 }
-else (name === "Joe") {
+;else (name === "Joe") {
   console.log("Expected the name to be Susan")
 }
 
